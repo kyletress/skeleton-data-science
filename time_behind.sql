c50d894d315cf10e6ee46335769c9b9de7d21608
Kyle Tress <kyle@kyletress.com>
2022-02-26 23:06:45 -0500

    partition by timesheet

--- time_behind.sql
+++ time_behind.sql
@@ -20,16 +20,13 @@ WHERE timesheet_id = 29
 ORDER BY status, rank, total_time ASC, bib ASC
 
 /* US ATHLETES */
-SELECT first_name, last_name, TRUNC(time_behind_1/100.0,2) AS time_behind_1, TRUNC(time_behind_3/100.0,2) AS time_behind_3, TRUNC(time_behind_6/100.0,2) AS time_behind_6 
+SELECT timesheet_id, CONCAT(first_name,' ', last_name) AS athlete, TRUNC(time_behind_1/100.0,2) time_behind_1, TRUNC(time_behind_3/100.0,2) time_behind_3, TRUNC(time_behind_6/100.0,2) time_behind_6, placement 
 FROM (
-	SELECT first_name, last_name, country_code, total_time - FIRST_VALUE(total_time)
-	OVER (PARTITION BY runs_count ORDER BY total_time ASC) AS time_behind_1,
-	total_time - NTH_VALUE(total_time, 3)
-	OVER (PARTITION BY runs_count ORDER BY total_time ASC) AS time_behind_3,
-	total_time - NTH_VALUE(total_time, 6)
-	OVER (PARTITION BY runs_count ORDER BY total_time ASC) AS time_behind_6,
-	rank()
-	OVER (ORDER BY status ASC, runs_count DESC, total_time ASC)
+	SELECT first_name, last_name, country_code, timesheet_id,
+	total_time - FIRST_VALUE(total_time) OVER (PARTITION BY timesheet_id, runs_count ORDER BY total_time ASC) time_behind_1,
+	total_time - NTH_VALUE(total_time, 3) OVER (PARTITION BY timesheet_id, runs_count ORDER BY total_time ASC) time_behind_3,
+	total_time - NTH_VALUE(total_time, 6) OVER (PARTITION BY timesheet_id, runs_count ORDER BY total_time ASC) time_behind_6,
+	RANK() OVER (PARTITION BY timesheet_id ORDER BY status ASC, runs_count DESC, total_time ASC) placement
 	FROM (
 		SELECT Entries.id, Entries.timesheet_id, Entries.athlete_id, Entries.status, Entries.bib, Entries.runs_count, sum(Runs.finish) 
 		AS total_time 
@@ -40,7 +37,8 @@ FROM (
 	) AS FinalRanks
 	LEFT JOIN Athletes
 	ON (Athletes.id = FinalRanks.athlete_id)
-	WHERE timesheet_id = 29
-	ORDER BY status, rank, total_time ASC, bib ASC
+	WHERE timesheet_id IN (29,30,33)
+	ORDER BY status, placement, total_time ASC, bib ASC
 	) AS usathletes
 WHERE country_code = 'US'
+ORDER BY timesheet_id ASC
