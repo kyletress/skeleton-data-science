2d0a61c726c9c0bfaf415d4b058445c111292cbe
Kyle Tress <kyle@kyletress.com>
2022-02-26 23:23:37 -0500

    Add circuit and track info

--- time_behind.sql
+++ time_behind.sql
@@ -1,32 +1,10 @@
-SELECT first_name, last_name, total_time - FIRST_VALUE(total_time)
-OVER (PARTITION BY runs_count ORDER BY total_time ASC) AS time_behind_1,
-total_time - NTH_VALUE(total_time, 3)
-OVER (PARTITION BY runs_count ORDER BY total_time ASC) AS time_behind_3,
-total_time - NTH_VALUE(total_time, 6)
-OVER (PARTITION BY runs_count ORDER BY total_time ASC) AS time_behind_6,
-rank()
-OVER (ORDER BY status ASC, runs_count DESC, total_time ASC)
+SELECT CONCAT(Tracks.name,' ', Circuits.name), CONCAT(first_name,' ', last_name) AS athlete, TRUNC(time_behind_1/100.0,2) time_behind_1, TRUNC(time_behind_3/100.0,2) time_behind_3, TRUNC(time_behind_6/100.0,2) time_behind_6, placement 
 FROM (
-	SELECT Entries.id, Entries.timesheet_id, Entries.athlete_id, Entries.status, Entries.bib, Entries.runs_count, sum(Runs.finish) 
-	AS total_time 
-	FROM Entries 
-	LEFT JOIN Runs 
-	ON (Entries.id = Runs.entry_id) 
-	GROUP BY Entries.id
-) AS FinalRanks
-LEFT JOIN Athletes
-ON (Athletes.id = FinalRanks.athlete_id)
-WHERE timesheet_id = 29
-ORDER BY status, rank, total_time ASC, bib ASC
-
-/* US ATHLETES */
-SELECT timesheet_id, CONCAT(first_name,' ', last_name) AS athlete, TRUNC(time_behind_1/100.0,2) time_behind_1, TRUNC(time_behind_3/100.0,2) time_behind_3, TRUNC(time_behind_6/100.0,2) time_behind_6, placement 
-FROM (
-	SELECT first_name, last_name, country_code, timesheet_id,
+	SELECT first_name, last_name, country_code, timesheet_id, circuit_id, track_id,
 	total_time - FIRST_VALUE(total_time) OVER (PARTITION BY timesheet_id, runs_count ORDER BY total_time ASC) time_behind_1,
 	total_time - NTH_VALUE(total_time, 3) OVER (PARTITION BY timesheet_id, runs_count ORDER BY total_time ASC) time_behind_3,
 	total_time - NTH_VALUE(total_time, 6) OVER (PARTITION BY timesheet_id, runs_count ORDER BY total_time ASC) time_behind_6,
-	RANK() OVER (PARTITION BY timesheet_id ORDER BY status ASC, runs_count DESC, total_time ASC) placement
+	RANK() OVER (PARTITION BY timesheet_id ORDER BY FinalRanks.status ASC, runs_count DESC, total_time ASC) placement
 	FROM (
 		SELECT Entries.id, Entries.timesheet_id, Entries.athlete_id, Entries.status, Entries.bib, Entries.runs_count, sum(Runs.finish) 
 		AS total_time 
@@ -37,8 +15,9 @@ FROM (
 	) AS FinalRanks
 	LEFT JOIN Athletes
 	ON (Athletes.id = FinalRanks.athlete_id)
-	WHERE timesheet_id IN (29,30,33)
-	ORDER BY status, placement, total_time ASC, bib ASC
+	LEFT JOIN Timesheets
+	ON (Timesheets.id = FinalRanks.timesheet_id)
+	WHERE circuit_id = 1
+	ORDER BY FinalRanks.status, placement, total_time ASC, bib ASC
 	) AS usathletes
-WHERE country_code = 'US'
-ORDER BY timesheet_id ASC
+LEFT JOIN Circuits
